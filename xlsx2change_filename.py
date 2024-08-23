6e02a388eb18a9d7fad678153158e780daf57418
NANNADA4 <coco96062233@icloud.com>
2024-08-23 17:06:13 +0900

    -

---
+++ xlsx2change_filename.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+#! 동일 파일명 handling 필요. 엑셀 L열에 파일명 대신 폴더 경로를 사용하는 방법
+
+
+def change_filename(excel_file_path, folder_path):
+    df = pd.read_excel(excel_file_path)
+
+    old_names = df['L'].tolist()
+    new_names_ext = df['G'].tolist()
+    new_names = [os.path.splitext(name)[0] for name in new_names_ext]
+    extensions = ['PDF', 'HWP', 'PBM']
+
+    for old_name, new_name in zip(old_names, new_names):
+        for ext in extensions:
+            old_file = os.path.join(folder_path, f"{old_name}.{ext}")
+            new_file = os.path.join(folder_path, f"{new_name}.{ext}")
+            if os.path.exists(old_file):
+                os.rename(old_file, new_file)
+                print(f"파일명을 변경했습니다: {old_file} -> {new_file}")
+            else:
+                print(f"파일이 존재하지 않습니다: {old_file}")
+
+
+if __name__ == "__main__":
+    excel_file_path = input("엑셀 파일 경로를 입력하세요: ")
+    folder_path = input("폴더 경로를 입력하세요: ")
+    change_filename(excel_file_path, folder_path)
+    print("작업이 완료되었습니다.")
