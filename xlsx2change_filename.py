a9f3fd4eeacd5e1bef9ebd849b730983e34e5eca
NANNADA4 <coco96062233@icloud.com>
2024-08-23 18:03:30 +0900

    done

--- xlsx2change_filename.py
+++ xlsx2change_filename.py
@@ -3,23 +3,29 @@ import os
 
 # 동일 파일명 handling 필요
 #! L열에 파일명 대신 폴더 경로 사용하는 것으로 확정
+#! ==> L열에 경로명, M열에 파일명 입력
 
 
 def change_filename(excel_file_path):
     df = pd.read_excel(excel_file_path)
 
-    old_names = df['L'].tolist()
-    new_names_ext = df['G'].tolist()
-    new_names = [os.path.splitext(name)[0] for name in new_names_ext]
+    old_names_dirname = df['Path'].tolist()  # L열 읽지 못함
+    old_names_filename = df['FileName'].tolist()  # M열 읽지 못함
+    old_names_with_dir = [os.path.join(dirname, filename) for dirname, filename in zip(
+        old_names_dirname, old_names_filename)]
+
+    new_names = df['PDF_NAME'].tolist()
+    new_names = [os.path.splitext(name)[0] for name in new_names]
     extensions = ['PDF', 'HWP', 'PBM']
 
-    for old_name, new_name in zip(old_names, new_names):
+    for old_name, new_name, old_dirname in zip(old_names_with_dir, new_names, old_names_dirname):
         for ext in extensions:
-            old_file = os.path.join(f"{old_name}.{ext}")
-            new_file = os.path.join(f"{new_name}.{ext}")
+            old_file = os.path.join("\\\\?\\", f"{old_name}.{ext}")
+            new_file_with_dir = os.path.join("\\\\?\\",
+                                             old_dirname, f"{new_name}.{ext}")
             if os.path.exists(old_file):
-                os.rename(old_file, new_file)
-                print(f"파일명을 변경했습니다: {old_file} -> {new_file}")
+                os.rename(old_file, new_file_with_dir)
+                print(f"파일명을 변경했습니다: {old_file} -> {new_file_with_dir}")
             else:
                 print(f"파일이 존재하지 않습니다: {old_file}")
 
