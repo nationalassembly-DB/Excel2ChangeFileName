b0d932b225d9755047e43b8a0b9c23b3254f3a0f
NANNADA4 <coco96062233@icloud.com>
2024-09-13 10:08:43 +0900

    done

---
+++ create_upload_excel.py
@@ -0,0 +1,22 @@
+
+from module.change_filename import change_filename
+from module.create_excel import create_excel
+
+
+def main():
+    """main 함수"""
+    print('원하시는 작업을 선택하세요')
+    select_input = input('1.엑셀 생성 2.파일명 변경 (번호 입력) 0.종료 : ')
+    if select_input == '1':
+        create_excel()
+    elif select_input == '2':
+        change_filename()
+    elif select_input == '0':
+        return
+    else:
+        print("다시 입력하세요\n")
+        main()
+
+
+if __name__ == "__main__":
+    main()
--- module/change_filename.py
+++ module/change_filename.py
@@ -2,21 +2,21 @@
 엑셀 파일을 사용하여 폴더명에 위치한 파일의 이름을 변경합니다
 """
 
+
 import os
 import pandas as pd
 
-# 동일 파일명 handling 필요
-#! L열에 파일명 대신 폴더 경로 사용하는 것으로 확정
-#! ==> L열에 경로명, M열에 파일명 입력
 
-
-def change_filename(excel_file_path):
+def change_filename():
     """
     엑셀파일을 사용하여 L열에 위치한 폴더명을 변경합니다
     엑셀파일 헤더 : YEAR,AUDITTYPE_CDB,COMMITTEE_ID,COMMITTEE_NAME,ORG_ID,
     ORG_NAME,PDF_NAME,HWP_NAME,PBM_NAME,BOOK_NAME,DIRECTORY_CDB,Path,FileName
     """
-    df = pd.read_excel(excel_file_path)
+    print("\n>>>>>>파일명 변경<<<<<<\n")
+    print("-"*24)
+    excel_path = input("엑셀 파일 경로를 입력하세요: ")
+    df = pd.read_excel(excel_path)
 
     old_names_dirname = df['Path'].tolist()  # L열 읽지 못함
     old_names_filename = df['FileName'].tolist()  # M열 읽지 못함
@@ -38,8 +38,4 @@ def change_filename(excel_file_path):
             else:
                 print(f"파일이 존재하지 않습니다: {old_file}")
 
-
-if __name__ == "__main__":
-    excel_path = input("엑셀 파일 경로를 입력하세요: ")
-    change_filename(excel_path)
     print("작업이 완료되었습니다.")
